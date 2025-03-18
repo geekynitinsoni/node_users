0d6226a3f9a1fdb340cbebd7ac443907c07eb08f
geekynitinsoni <nitinsoni0899@gmail.com>
2025-03-18 12:40:36 +0530

    created remote mysql db in db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ require("dotenv").config(); // load env files
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: "sql12.freesqldatabase.com",
+  user: "sql12768264",
   password: process.env.DB_PASS,
-  database: "testdb",
+  database: "sql12768264",
 });
 
 db.connect((err) => {
